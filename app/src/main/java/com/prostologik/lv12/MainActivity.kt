66bc31803a20c1e44bd526a662da5c9033083a01
andriydanylovych <andriy.danylovych@gmail.com>
2025-05-11 12:29:59 +0300

    appBarMain.fab INVISIBLE

--- app/src/main/java/com/prostologik/lv12/MainActivity.kt
+++ app/src/main/java/com/prostologik/lv12/MainActivity.kt
@@ -9,10 +9,11 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 import com.prostologik.lv12.databinding.ActivityMainBinding
-import com.prostologik.lv12.ui.home.HomeFragment
+//import com.google.android.material.snackbar.Snackbar
+//import com.prostologik.lv12.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -41,13 +42,15 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
 
-        binding.appBarMain.fab.setOnClickListener {
-            view ->
-            val note = HomeFragment.buildHomeFragmentInfo()//homeFragmentInfo
-            Snackbar.make(view, note, Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
+//        val temp = binding.appBarMain.fab
+        binding.appBarMain.fab.visibility = FloatingActionButton.INVISIBLE
+//        binding.appBarMain.fab.setOnClickListener {
+//            view ->
+//            val note = HomeFragment.buildHomeFragmentInfo()//homeFragmentInfo
+//            Snackbar.make(view, note, Snackbar.LENGTH_LONG)
+//                .setAction("Action", null)
+//                .setAnchorView(R.id.fab).show()
+//        }
 
     }
 
